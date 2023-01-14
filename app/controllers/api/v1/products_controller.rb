ae393b6b49137948615c44bd7f85778230b96527
YuichiMasumiya <masumiya@manpuku.farm>
2023-01-14 16:40:42 +0900

    とりあえずテストがパスする形に一旦整形

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,7 +5,33 @@ module Api
     class ProductsController < ApplicationController
       def index
         @products = Product.order(created_at: :desc)
-        render json: {}
+        render json: result_json
+      end
+
+      private
+
+      def result_json
+        {
+          data: { products: products_json }
+        }
+      end
+
+      def products_json
+        @products.map do |product|
+          {
+            id: product.id,
+            variants: variants_json(product)
+          }
+        end
+      end
+
+      def variants_json(product)
+        variants = Variant.where(product: product).order(created_at: :desc)
+        variants.map do |variant|
+          {
+            id: variant.id
+          }
+        end
       end
     end
   end
