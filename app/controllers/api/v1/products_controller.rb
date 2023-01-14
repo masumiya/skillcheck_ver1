9d2ef747deb9c8ca95a564a417f2e5f79d7b6247
YuichiMasumiya <masumiya@manpuku.farm>
2023-01-14 20:06:24 +0900

    N+1を解決

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,34 +4,17 @@ module Api
   module V1
     class ProductsController < ApplicationController
       def index
-        @products = Product.order(created_at: :desc)
-        render json: result_json
+        @products = Product.order(created_at: :desc).eager_load(:variants)
+
+        render json: result_json, include: [:variants], status: :ok
       end
 
       private
 
       def result_json
         {
-          data: { products: products_json }
+           data:  { products: @products }
         }
-      end
-
-      def products_json
-        @products.map do |product|
-          {
-            id: product.id,
-            variants: variants_json(product)
-          }
-        end
-      end
-
-      def variants_json(product)
-        variants = Variant.where(product: product).order(created_at: :desc)
-        variants.map do |variant|
-          {
-            id: variant.id
-          }
-        end
       end
     end
   end
